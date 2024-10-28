174bd4cda46088e8160161987ddb6450a3e67688
Qishun Zhong (WICRESOFT NORTH AMERICA LTD) <v-qzhong@microsoft.com>
2024-10-28 17:05:33 +0800

    reset TestPageLabel.cs

--- DataAutoFramework/TestCases/TestPageLabel.cs
+++ DataAutoFramework/TestCases/TestPageLabel.cs
@@ -1,7 +1,6 @@
 using NUnit.Framework.Legacy;
 using NUnit.Framework;
 using Microsoft.Playwright;
-using System.Text.Json;
 using System.Text.RegularExpressions;
 
 namespace DataAutoFramework.TestCases
@@ -9,18 +8,19 @@ namespace DataAutoFramework.TestCases
     public class TestPageLabel
     {
         public static List<string> TestLinks { get; set; }
-        public static Dictionary<string, string> SpecialLinks { get; set; }
 
         static TestPageLabel()
         {
-            TestLinks = JsonSerializer.Deserialize<List<string>>(File.ReadAllText("appsettings.json")) ?? new List<string>();
-
-            SpecialLinks = new Dictionary<string, string>();
-
-            SpecialLinks.Add("Read in English", "https://learn.microsoft.com/en-us/python/api/overview/azure/app-configuration?view=azure-python");
-            SpecialLinks.Add("our contributor guide", "https://github.com/Azure/azure-sdk-for-python/blob/main/CONTRIBUTING.md");
-            // SpecialLinks.Add("English (United States)", "/en-us/locale?target=https%3A%2F%2Flearn.microsoft.com%2Fen-us%2Fpython%2Fapi%2Foverview%2Fazure%2Fapp-configuration%3Fview%3Dazure-python");
-            SpecialLinks.Add("Privacy", "https://go.microsoft.com/fwlink/?LinkId=521839");
+            TestLinks = new List<string>
+            {
+                "https://learn.microsoft.com/en-us/python/api/overview/azure/app-configuration?view=azure-python",
+                "https://learn.microsoft.com/en-us/python/api/overview/azure/appconfiguration-readme?view=azure-python",
+                "https://learn.microsoft.com/en-us/python/api/azure-appconfiguration/azure.appconfiguration?view=azure-python",
+                "https://learn.microsoft.com/en-us/python/api/azure-appconfiguration/azure.appconfiguration.aio?view=azure-python",
+                "https://learn.microsoft.com/en-us/python/api/azure-appconfiguration/azure.appconfiguration.aio.azureappconfigurationclient?view=azure-python",
+                "https://learn.microsoft.com/en-us/python/api/azure-appconfiguration/azure.appconfiguration.azureappconfigurationclient?view=azure-python",
+                "https://learn.microsoft.com/en-us/python/api/azure-mgmt-batch/azure.mgmt.batch.models.azureresource"
+            };
         }
 
 
@@ -29,7 +29,7 @@ namespace DataAutoFramework.TestCases
         public async Task TestExtraLabel(string testLink)
         {
             var errorList = new List<string>();
-            var labelList = new List<string> { 
+            var labelList = new List<string> {
                 "<br",
                 "<h1",
                 "<h2",
@@ -90,7 +90,7 @@ namespace DataAutoFramework.TestCases
             var paragraphs = await page.Locator("p").AllInnerTextsAsync();
             var tableContents = new List<string>();
             var tableCount = await page.Locator("table").CountAsync();
-            
+
             if (paragraphs != null)
             {
                 foreach (var paragraph in paragraphs)
@@ -117,7 +117,7 @@ namespace DataAutoFramework.TestCases
                 {
                     errorList.Add(match.Value);
                 }
-            }            
+            }
 
             ClassicAssert.Zero(errorList.Count, testLink + " has unnecessary symbols:\n" + string.Join("\n", errorList));
         }
